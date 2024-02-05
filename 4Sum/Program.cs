ac1376e3cc434b8115486d282b37e58f70029b07
Serhii Chervonyi <s.chervonyi@outlook.com>
2024-02-05 08:21:55 +0000

    4 sum solution

---
+++ 4Sum/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace _4Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var arr2 = new[] { -2, -1, -1, 1, 1, 2, 2 };
+            var arr3 = new[] { 2, 2, 2, 2, 2 };
+            var arr5 = new[] { 1000000000, 1000000000, 1000000000, 1000000000 };
+            // -1, 0, [1, 2, -1, -4]  ...
+            // -1, 2, 
+            var arr4 = new[] { 0, 0, 0, 0 };
+
+            var target = -294967296;
+            var res = s.FourSum(arr2, target);
+
+            foreach (var item in res)
+            {
+                Console.WriteLine(string.Join(',', item));
+            }
+        }
+    }
+
+    public class Solution
+    {
+        public IList<IList<int>> FourSum(int[] nums, int target)
+        {
+            var result = new List<int[]>();
+            Array.Sort(nums);
+
+            if (nums[nums.Length - 1] > target)
+            {
+                return result.ToArray();
+            }
+
+            int left, right;
+            for (int start1 = 0; start1 < nums.Length - 3; start1++)
+            {
+                if (start1 > 0 && nums[start1] == nums[start1 - 1]) continue; // skipping repeated numbers to avoid repeating triples
+
+                for (int start2 = start1 + 1; start2 < nums.Length - 2; start2++)
+                {
+                    if (start2 > start1 + 1 && nums[start2] == nums[start2 - 1]) continue; // skipping repeated numbers to avoid repeating triples
+
+                    left = start2 + 1;
+                    right = nums.Length - 1;
+
+                    while (left < right)
+                    {
+                        //long numberSum = (long)number[x] + (long)number[y] + (long)number[leftPointer] + (long)number[rightPointer];
+
+                        long sum = (long)nums[start1] + (long)nums[start2] + (long)nums[right] + (long)nums[left];
+
+                        if (sum == target)
+                        {
+                            result.Add(new[] { nums[start1], nums[start2], nums[right], nums[left] });
+                            while (left < right && nums[left] == nums[left + 1]) left++; // skipping repeated numbers to avoid repeating triples
+                            while (left < right && nums[right] == nums[right - 1]) right--; // skipping repeated numbers to avoid repeating triples
+                            left++;
+                            right--;
+                        }
+                        else if (sum < target)
+                            left++;
+                        else
+                            right--;
+                    }
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ LetterCombinationsOfPhoneNumber/Program.cs
@@ -0,0 +1,105 @@
+﻿namespace LetterCombinationsOfPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+
+            var result = s.LetterCombinations("23");
+            Console.WriteLine(string.Join(',', result));
+        }
+    }
+
+    public class Solution
+    {
+        public IList<string> LetterCombinations(string digits)
+        {
+            var dictionary = BuildDictionary();
+
+            var charListArrays = new List<char[]>();
+            foreach (var digit in digits)
+            {
+                charListArrays.Add(dictionary[digit]);
+            }
+
+            if (charListArrays.Count == 0)
+            {
+                return new List<string>();
+            }
+
+            return AddElements(charListArrays, string.Empty, 0);
+        }
+
+        private IList<string> AddElements(List<char[]> charListArrays, string prefix, int index)
+        {
+            var result = new List<string>();
+            if (charListArrays.Count - 1 == index)
+            {
+                foreach (var charItem in charListArrays[index])
+                {
+                    result.Add($"{prefix}{charItem}");
+                }
+            }
+            else
+            {
+                foreach (var charItem in charListArrays[index])
+                {
+                    var newPrefix = $"{prefix}{charItem}";
+                    result.AddRange(AddElements(charListArrays, newPrefix, index + 1));
+                }
+            }
+
+            return result;
+        }
+
+        private Dictionary<char, char[]> BuildDictionary()
+        {
+            return new Dictionary<char, char[]> {
+                //{'1',  new[] {''} },
+                {'2',  new[] {'a', 'b', 'c' } },
+                {'3',  new[] {'d', 'e', 'f' } },
+                {'4',  new[] {'g', 'h', 'i' } },
+                {'5',  new[] {'j', 'k', 'l' } },
+                {'6',  new[] {'m', 'n', 'o' } },
+                {'7',  new[] {'p', 'q', 'r', 's' } },
+                {'8',  new[] {'t', 'u', 'v' } },
+                {'9',  new[] {'w', 'x', 'y', 'z' } },
+            };
+        }
+    }
+
+    public class Solution2
+    {
+        Dictionary<char, char[]> keypad = new Dictionary<char, char[]> {{'2', new char[]{'a', 'b', 'c'}},
+    {'3', new char[]{'d', 'e', 'f'}}, {'4', new char[] {'g', 'h', 'i'}},
+    {'5', new char[] {'j', 'k', 'l'}}, {'6', new char[] {'m', 'n', 'o'}},
+    {'7', new char[] {'p', 'q', 'r', 's'}}, {'8', new char[] {'t', 'u', 'v'}},
+    {'9', new char[] {'w', 'x', 'y', 'z'}}};
+
+        public void AddCombination(string curr, string digits, int index, IList<string> list)
+        {
+            if (index >= digits.Length) list.Add(curr);
+            else
+            {
+                char[] map = keypad[digits[index]];
+
+                for (int i = 0; i < map.Length; i++)
+                {
+                    string newCurr = curr + map[i];
+
+                    AddCombination(newCurr, digits, index + 1, list);
+                }
+            }
+        }
+
+        public IList<string> LetterCombinations(string digits)
+        {
+            IList<string> combinations = new List<string>();
+
+            if (digits.Length > 0) AddCombination("", digits, 0, combinations);
+
+            return combinations;
+        }
+    }
+}
