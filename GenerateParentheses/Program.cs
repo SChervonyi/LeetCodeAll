7eff2e579d461e6e62d3bf91aed90b7b9ba20605
Serhii Chervonyi <s.chervonyi@outlook.com>
2024-02-16 07:53:37 +0000

    Add solutions

---
+++ GenerateParentheses/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace GenerateParentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var s  = new Solution();
+            var result = s.GenerateParenthesis(3);
+            Console.WriteLine(string.Join(",", result));
+        }
+    }
+
+    public class Solution
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            IList<string> res = new List<string>();
+            Helper(res, "(", 1, 0, n);
+            return res;
+        }
+
+        public void Helper(IList<string> res, string curr, int open, int close, int n)
+        {
+            if (curr.Length == 2 * n)
+            {
+                res.Add(curr);
+                return;
+            }
+            if (open < n)
+            {
+                Helper(res, curr + "(", open + 1, close, n);
+            }
+            if (close < open)
+            {
+                Helper(res, curr + ")", open, close + 1, n);
+            }
+        }
+
+    }
+}
---
+++ SwapNodesInPairs/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace SwapNodesInPairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
+
+            var s = new Solution();
+
+            var result = s.SwapPairs(list1);
+            Console.WriteLine(result);
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode? SwapPairs(ListNode? head)
+        {
+            var next = head?.next;
+
+            if (head == null || next == null) return next ?? head;
+
+            var nextNext = next.next;
+
+            head.next = SwapPairs(nextNext);
+            next.next = head;
+            return next;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode? next;
+        public ListNode(int val = 0, ListNode? next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
